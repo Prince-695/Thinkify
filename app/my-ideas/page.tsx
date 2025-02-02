f43eeb6f59709e12e4fc88fab2f88ac6a2f60782
Prince Rathod <rathodprince411@gmail.com>
2025-02-02 08:58:31 +0530

    removeNavFromMyIdeas

--- app/my-ideas/page.tsx
+++ app/my-ideas/page.tsx
@@ -11,14 +11,7 @@ const MyIdeasPage = async () => {
   return (
     <div className="p-6">
       <h1 className="mt-5 text-4xl font-bold text-purple-600 mb-[-25px]">My Ideas</h1>
-      <div className='h-screen mr-[330px] my-auto'>
-
       <IdeasGrid />
-      </div>
-      <div className='ml-[1150px] top-0 fixed '>
-      <Sidebar {...currentUser}  />
-      
-    </div>
       </div>
   );
 };
